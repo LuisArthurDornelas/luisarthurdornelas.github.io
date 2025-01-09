f79abc0dd88b772171353182fd0858befc26a085
Kalil Cabral <kbcv@cesar.school>
2025-01-09 14:56:52 -0300

    Add site contents

---
+++ src/lib/components/entries.svelte
@@ -0,0 +1,18 @@
+<script>
+	import allEntries from '$lib/assets/data.yml';
+	const { type } = $props();
+	const entries = allEntries.filter((e) => type.includes(e.type));
+</script>
+
+{#each entries as e}
+	<article>
+		{#if e.title}
+			<h3>{e.title}</h3>
+		{/if}
+		<p><b>{e.org}, {e.date}</b></p>
+		<p>{e.desc}</p>
+		{#if e.uses}
+			<p><i>{e.uses}</i></p>
+		{/if}
+	</article>
+{/each}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,66 @@
 <script>
-	import data from '$lib/data.yml';
+	import Entries from '$lib/components/entries.svelte';
+	import bgImg from '$lib/assets/bg.jpg';
+	import profilePic from '$lib/assets/profile.jpg';
 </script>
 
-<pre>{JSON.stringify(data, null, 4)}</pre>
+<header style="background: url({bgImg}); color: white">
+	<h1>Luís Arthur Dornelas</h1>
+	<p>JUNIOR DEVELOPER</p>
+	<img src={profilePic} alt="Foto de Luís" style="width: 200px" />
+</header>
+<main>
+	<div class="rcol">
+		<section id="contact">
+			<h2>Contato</h2>
+			<p><b>Email:</b> ladccc@cesar.school</p>
+		</section>
+		<section id="about">
+			<h2>Sobre mim</h2>
+			<p>
+				Desenvolvedor júnior com experiência em trabalhar com equipes multidisciplinares na ideação
+				e realização de projetos.
+			</p>
+			<p>
+				<i>
+					Tenho interesse em atuar com programadores mais experientes com a finalidade de
+					desenvolver projetos desafiadores utilizando novas tecnologias.
+				</i>
+			</p>
+		</section>
+		<section id="skills">
+			<h2>Habilidades</h2>
+			<ul>
+				<li>Python</li>
+				<li>C/C#</li>
+				<li>JavaScript/TypeScript</li>
+				<li>Ruby on Rails</li>
+				<li>Docker</li>
+				<li>HTML/CSS</li>
+				<li>Angular</li>
+				<li>Java</li>
+				<li>.NET</li>
+				<li>SQL</li>
+				<li>AWS</li>
+				<li>Linux</li>
+				<li>Windows</li>
+				<li>Metodologias Ágeis</li>
+				<li>Pacote Office</li>
+			</ul>
+		</section>
+	</div>
+	<div class="lcol">
+		<section id="experiences">
+			<h2>Atuação e projetos</h2>
+			<Entries type={['job', 'proj']} />
+		</section>
+		<section id="formations">
+			<h2>Formação e certificações</h2>
+			<Entries type={['edu', 'cert']} />
+		</section>
+	</div>
+</main>
+<footer>
+	<p>Website desenhado e desenvolvido por Kalil Cabral com Svelte</p>
+	<p>Copyright ©2024-2025 Luís Arthur Dornelas | Todos os direitos reservados</p>
+</footer>
